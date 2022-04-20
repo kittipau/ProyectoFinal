5bdbb45bae03ce193f9767a12d6639a2444e9cf1
paula <punibasol01@educantabria.es>
2022-04-20 14:15:13 +0200

    COSITAS NUEVAS

---
+++ appBiaf/src/main/java/com/example/appBiaf/Cliente.java
@@ -0,0 +1,53 @@
+package com.example.appBiaf;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class Cliente extends Thread {
+
+    public Cliente() {
+    }
+
+    @Override
+    public void run() {
+        try {
+
+            String equipoServidor = "127.0.0.1";
+            int puertoServidor = 30500;
+
+            /** Se abre un socket en el cliente, y dicho socket establece
+             "una conexión con el socket del servidor situado en el puerto 30500 de la 127.0.0.1" */
+            Socket socketCliente = new Socket(equipoServidor, puertoServidor);
+
+
+            //Se cierra el cliente
+            socketCliente.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+        public void gestionarComunicacion (Socket socketCliente, Object objeto) {
+            try {
+                //El cliente contruye el objetco y lo envia al servidor
+                ObjectOutputStream oos = new ObjectOutputStream(socketCliente.getOutputStream());
+                oos.writeObject(objeto);
+
+                //El cliente recibe un objeto del servidor
+                ObjectInputStream ois = new ObjectInputStream(socketCliente.getInputStream());
+                // Se lee el objeto recibido
+                objeto  = (Object) ois.readObject();
+
+                //Se cierra la comunicación
+                ois.close();
+                oos.close();
+            } catch (IOException | ClassNotFoundException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+    }
--- appBiaf/src/main/java/com/example/appBiaf/InicioSesionFragment.java
+++ appBiaf/src/main/java/com/example/appBiaf/InicioSesionFragment.java
@@ -1,17 +1,28 @@
 package com.example.appBiaf;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.appBiaf.databinding.FragmentHomeBinding;
 import com.example.appBiaf.databinding.FragmentInicioSesionBinding;
 
+import org.w3c.dom.Text;
+
 public class InicioSesionFragment extends Fragment {
+
+
+
 
 
     private FragmentInicioSesionBinding binding;
@@ -20,7 +31,34 @@ public class InicioSesionFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentInicioSesionBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        TextView text = root.findViewById(R.id.textView2);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Crear fragmento de tu clase
+                Fragment fragment = (InicioSesionFragment) getParentFragmentManager().findFragmentById(R.id.fragment_container_view_tag);
+                // Obtener el administrador de fragmentos a través de la actividad
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                // Definir una transacción
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                // Remplazar el contenido principal por el fragmento
+                fragmentTransaction.replace(R.id.fragment_container_view_tag, fragment);
+                fragmentTransaction.addToBackStack(null);
+                // Cambiar
+                fragmentTransaction.commit();
+            }
+        });
+
+
+
+
         return root;
+
+
+
+
     }
     @Override
     public void onDestroyView() {
